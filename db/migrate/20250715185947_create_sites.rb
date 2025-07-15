11e4e8a17a5b7bb3d77244e55b50b6cdfffc9825
Slava Haikin <vyachowski@gmail.com>
2025-07-15 22:06:51 +0300

    feat: add correct references

--- db/migrate/20250715185947_create_sites.rb
+++ db/migrate/20250715185947_create_sites.rb
@@ -1,7 +1,7 @@
 class CreateSites < ActiveRecord::Migration[8.0]
   def change
     create_table :sites do |t|
-      t.integer :user_id, null: false
+      t.references :user, null: false, foreign_key: true
       t.string :url, null: false
 
       t.timestamps
--- db/migrate/20250715190153_create_uptime_records.rb
+++ db/migrate/20250715190153_create_uptime_records.rb
@@ -1,7 +1,7 @@
 class CreateUptimeRecords < ActiveRecord::Migration[8.0]
   def change
     create_table :uptime_records do |t|
-      t.integer :site_id, null: false
+      t.references :site, null: false, foreign_key: true
       t.boolean :available, null: false
       t.integer :ping, null: false
 
