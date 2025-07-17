8a627d5d3915b429a97f17fc677c2ef4368e37a0
Slava Haikin <vyachowski@gmail.com>
2025-07-17 20:12:21 +0300

    feat: add site validation

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,5 +1,8 @@
 class Site < ApplicationRecord
-    belongs_to :User
+    belongs_to :user
 
     has_many :uptime_records, dependent: :destroy
+
+    validates :user_id, presence: true
+    validates :url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp }
 end
