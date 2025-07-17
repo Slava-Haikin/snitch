a1636d5092cb6c91dbf97ee0ad591d4f8746216a
Slava Haikin <vyachowski@gmail.com>
2025-07-17 20:17:21 +0300

    feat: add uptime record validation

--- app/models/uptime_record.rb
+++ app/models/uptime_record.rb
@@ -1,5 +1,6 @@
 class UptimeRecord < ApplicationRecord
     belongs_to :site
 
-
+    validates :available, inclusion: { in: [ true, false ] }
+    validates :ping, presence: true, numericality: { only_integer: true }
 end
